77e9472331d2fc48f3d6b70131e83c170b9393bf
Diego Fabricio <difagume@gmail.com>
2018-09-29 17:02:40 -0500

    Modulos y componentes (12 -130)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuario/usuarios.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        UsuariosModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
