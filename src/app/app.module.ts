5ce2a29cc37e899ccc7faa985919dc67a4562831
Diego Fabricio <difagume@gmail.com>
2018-09-29 17:20:36 -0500

    Rutas de la aplicación (12 - 131)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuario/usuarios.module';
@@ -10,6 +11,7 @@ import { UsuariosModule } from './usuarios/usuario/usuarios.module';
     ],
     imports: [
         BrowserModule,
+        AppRoutingModule,
         SharedModule,
         UsuariosModule
     ],
