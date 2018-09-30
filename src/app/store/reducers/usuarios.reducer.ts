1fc4bcb5353873d95e6d0a81d378e8c01c71871c
Diego Fabricio <difagume@gmail.com>
2018-09-29 19:30:19 -0500

    Reducer de usuarios

---
+++ src/app/store/reducers/usuarios.reducer.ts
@@ -0,0 +1,45 @@
+import { Usuario } from '../../models/usuario.model';
+import { CARGAR_USUARIOS, usuariosAcciones, CARGAR_USUARIOS_SUCCESS, CARGAR_USUARIOS_FAIL } from '../actions';
+
+export interface UsuariosState {
+    users: Usuario[];
+    loaded: boolean;
+    loading: boolean;
+    error: any;
+}
+
+const estadoInicial: UsuariosState = {
+    users: [],
+    loaded: false,
+    loading: false,
+    error: null
+};
+
+export function usuariosReducer(state = estadoInicial, action: usuariosAcciones): UsuariosState {
+    switch (action.type) {
+        case CARGAR_USUARIOS:
+            return {
+                ...state,
+                loading: true
+            };
+
+        case CARGAR_USUARIOS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                users: [...action.usuarios]
+            };
+
+        case CARGAR_USUARIOS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                loaded: false,
+                error: action.payload
+            };
+
+        default:
+            return state;
+    }
+}
