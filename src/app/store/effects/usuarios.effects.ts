bd53e42dd09be8e286fab7205ba0c04391c84dbd
Diego Fabricio <difagume@gmail.com>
2018-09-29 20:25:07 -0500

    Effects de usuarios - parte 2 (13 - 146)

--- src/app/store/effects/usuarios.effects.ts
+++ src/app/store/effects/usuarios.effects.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { CARGAR_USUARIOS } from '../actions';
+import { map, switchMap } from 'rxjs/operators';
+import { UsuarioService } from '../../services/usuario.service';
+import { CargarUsuariosSuccess, CARGAR_USUARIOS } from '../actions';
 
 @Injectable()
 export class UsuariosEffects {
 
-    constructor(private actions$: Actions) { }
+    constructor(private actions$: Actions,
+        private usuarioService: UsuarioService) { }
 
     @Effect()
-    cargarUsuarios$ = this.actions$.ofType(CARGAR_USUARIOS);
+    cargarUsuarios$ = this.actions$.ofType(CARGAR_USUARIOS)
+        .pipe(
+            switchMap(() => {
+                return this.usuarioService.getUsers()
+                    .pipe(
+                        map(users => new CargarUsuariosSuccess(users))
+                    );
+            })
+        );
 
 }
